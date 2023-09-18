a81a899f386182254197916f896528a2dbf10b9e
luizpbello <luizpaulopinto25@gmail.com>
2023-09-17 22:45:29 -0300

    style: mobile reputação

--- src/pages/Home/Reputacao/style.ts
+++ src/pages/Home/Reputacao/style.ts
@@ -3,10 +3,16 @@ import styled from 'styled-components'
 export const Container = styled.section`
   padding: 3rem 5rem;
 `
-export const Content = styled.section`
+export const Content = styled.section` 
   display: flex;
   justify-content: center;
   gap: 4rem;  
+
+  @media screen and (max-width: ${(props) => props.theme.breakpoints.mobile}) {
+    flex-direction: column-reverse;
+    align-items: center;
+    gap:1rem;
+  }
 `
 
 export const ReputacaoText = styled.div`
@@ -17,6 +23,7 @@ export const ReputacaoText = styled.div`
   @media screen and (max-width: ${(props) => props.theme.breakpoints.tablet}) {
   }
   @media screen and (max-width: ${(props) => props.theme.breakpoints.mobile}) {
+    max-width: 300px;
   }
 `
 
@@ -27,6 +34,11 @@ export const ReputacaoCard = styled.div`
   border-radius: 8px;
   &.melhores {
     background: #009c4f;
+    @media screen and (min-width: ${(props) => props.theme.breakpoints.mobile}) {
+    width: 320px;
+    background: ${(props) => props.theme.colors.primary.light};
+    margin-bottom: 2rem;
+   }
   }
   &.piores {
     background: ${(props) => props.theme.colors.grey.normal};
@@ -64,6 +76,10 @@ export const ReputacaoCardTitle = styled.h2`
     line-height: 160%; /* 32px */
     letter-spacing: 0.15px;
     padding: 16px 20px
+  }
+  @media screen and (min-width: ${(props) => props.theme.breakpoints.mobile}) {
+    font-size: ${(props) => props.theme.typography.desktop.headline6.fontSize};
+    max-width: 100%;
   }
 `
 
@@ -161,6 +177,7 @@ export const ReputacaoTextTitle = styled.h2`
   }
   @media screen and (max-width: ${(props) => props.theme.breakpoints.mobile}) {
     font-size: ${(props) => props.theme.typography.mobile.headline2.fontSize};
+    max-width: 80%;
 
   }
 `
@@ -178,5 +195,9 @@ export const ReputacaoTextSubTitle = styled.p`
   @media screen and (max-width: ${(props) => props.theme.breakpoints.tablet}) {
   }
   @media screen and (max-width: ${(props) => props.theme.breakpoints.mobile}) {
+    max-width: 300px;
+    font-size: ${(props) => props.theme.typography.desktop.body2.fontSize};
+    font-weight: lighter;
+
   }
 `
