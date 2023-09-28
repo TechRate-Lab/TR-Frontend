a1f6ee1f5cb2d4afee7a2a89cc0f5eafe50e8fd3
Luiz Bello <88148849+luizpbello@users.noreply.github.com>
2023-09-28 00:14:44 +0000

    fix: ajustes para deploy vercel

--- src/configs/routes/router.tsx
+++ src/configs/routes/router.tsx
@@ -1,17 +1,16 @@
-import { createBrowserRouter} from 'react-router-dom'
-import Home from '../../pages/Home'
-import CadastroPage from '../../pages/Cadastro'
-
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../../pages/Home";
+import CadastroPage from "../../pages/Cadastro";
 
 const router = createBrowserRouter([
-    {
-    path:'/',
-    element: <Home />
-    },
-    {
-        path:'/cadastro',
-        element:<CadastroPage />
-    }
-])
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/cadastro",
+    element: <CadastroPage />,
+  },
+]);
 
-export default router
+export default router;
